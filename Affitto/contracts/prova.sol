341bd24346b2f1789cc73b02d3d4148951e2d894
bananna1 <annagiacomello99@gmail.com>
2023-06-21 12:54:59 +0200

    Fatte cose

--- Affitto/contracts/prova.sol
+++ Affitto/contracts/prova.sol
@@ -7,22 +7,26 @@ contract Prova {
 
     struct contract_instance {
         uint contract_id;
-        string room_id;
+        uint room_id;
         bool student_paid;
         bool renter_paid;
+        uint amt_paid;
         address payable student;
         address payable renter;
     }
 
     struct room {
-        string id;
+        uint id;
         address owner;
         bool occupied;
+        uint deposit;
     }
 
     // the bool variable already_init is used to check if the couple (key, struct) has already been explicitely set (to )
     struct user {
         Role role;
+        uint[] room_record;
+        uint num_rooms;
         bool already_init;
     }
 
@@ -33,54 +37,52 @@ contract Prova {
 
     address payable contract_address = payable(address(this));
     mapping(uint => contract_instance) contract_record;         // associa a ogni id la rispettiva struct di contratto
-    mapping(address => uint[]) address_record;                  // associa a ogni indirizzo gli id di contratto a cui sta partecipando
-    mapping(address => user) user_role;                        // associa a ogni indirizzo il suo ruolo (student: 0, renter: 1)
-    mapping(address => room[]) renters_rooms;                  // associa a ogni renter un array che contiene l'id (o NFT, chissà) delle sue stanze
-    mapping(string => room) rooms_record;
-    uint[] contract_ids;    
-    uint conversion_rate = 586099570929370; 
-    uint caparra = 200;                                         // si può stabilire una somma fissa per la caparra oppure farla scegliere al renter (?)
+    mapping(address => user) user_info;                        // associa a ogni indirizzo il suo ruolo (student: 0, renter: 1)
+    mapping(address => uint[]) renters_rooms;                  // associa a ogni renter un array che contiene l'id (o NFT, chissà) delle sue stanze
+    mapping(uint => room) rooms_record;    
+    uint conversion_rate = 586099570929370;                     // DA CHIEDERE ALL'ESTERNO?
     uint num_contracts;                                         // var globale che incremento a ogni nuovo contratto e la uso come id
 
     constructor() {
         //contract_record = new mapping(uint => contract_instance); // non chiaro come si inizializza
         num_contracts = 0;
-        contract_ids = new uint[](0); // capire che dimensione mettere, se fa un resize automatico, ecc
     }
 
     function create_user(bool role_id) public {
         // Doesn't allow double registrations
-        require(!user_role[msg.sender].already_init, "User already registered");
-
+        require(!user_info[msg.sender].already_init, "User already registered");
+        uint256[] memory a = new uint256[](0);
         // false --> student, true --> renter
         if (role_id) {
-            user_role[msg.sender] = user({role: Role.Renter, already_init: true});
+            user_info[msg.sender] = user({role: Role.Renter, room_record: a, num_rooms: 0, already_init: true});
         }
         else {
-            user_role[msg.sender] = user({role: Role.Student, already_init: true});
+            user_info[msg.sender] = user({role: Role.Student, room_record: a, num_rooms: 0, already_init: true});
         }
         
     }
 
-    function register_rooms(string[] calldata rooms) public {
+    function register_rooms(uint[] memory rooms, uint[] memory deposits) public {
         // controlliamo che il chiamante sia un renter
-        require(user_role[msg.sender].role == Role.Renter, "You are not a renter therefore you're not allowed to register rooms");
-        
+        require(user_info[msg.sender].role == Role.Renter, "You are not a renter therefore you're not allowed to register rooms");
+        require(rooms.length == deposits.length, "Inconsistent information provided.");
         uint l = rooms.length;
-        room[] memory new_rooms_array = new room[](l);
-         
+        user_info[msg.sender].num_rooms = l; 
         // TODO: VERIFICARE CHE IL RENTER SIA EFFETTIVAMENTE IL PROPRIETARIO DELLE STANZE (sia fa una zkp nelle nft? chi lo sa)
 
         for (uint i = 0; i < l; i++) {
-            room memory new_room = room({id: rooms[i], owner: msg.sender, occupied: false});
-            new_rooms_array[i] = new_room;
+            room memory new_room = room({id: rooms[i], owner: msg.sender, occupied: false, deposit: deposits[i]});
+            renters_rooms[msg.sender][i] = rooms[i];
+            rooms_record[new_room.id] = new_room;
         }
-
-        renters_rooms[msg.sender] = new_rooms_array;
+        
     }   
 
     // If the student has his heart already set on a specific room 
-    function inizialize(address payable renter, string calldata room_id) public {
+    function inizialize(address payable renter, uint room_id) public {
+        require(check_user_already_registered(msg.sender), "You are not an authorized user");
+        require(check_user_already_registered(renter), "Renter is not an authorized user");
+
         address payable student = payable(msg.sender);
         
         // check che lo studente non sia già in una "istanza" di contratto o che la stanza non sia già occupata
@@ -93,25 +95,26 @@ contract Prova {
         uint new_id = num_contracts;
         num_contracts++;
         rooms_record[room_id].occupied = true;
-        contract_instance memory instance = contract_instance({contract_id: new_id, room_id: room_id, student_paid: false, renter_paid: false, student: student, renter: renter});
+        contract_instance memory instance = contract_instance({contract_id: new_id, room_id: room_id, student_paid: false, renter_paid: false, amt_paid: 0, student: student, renter: renter});
         contract_record[new_id] = instance;
-        contract_ids.push(new_id);
-        address_record[student].push(new_id);
-        address_record[renter].push(new_id);
+        user_info[student].room_record.push(new_id);
+        user_info[renter].room_record.push(new_id);
     }
 
     // if the student doesn't care about which room he gets assigned
     function inizialize(address payable renter) public {
+        require(check_user_already_registered(msg.sender), "You are not an authorized user");
+        require(check_user_already_registered(renter), "Renter is not an authorized user");
         address payable student = payable(msg.sender);
-        room[] memory rooms_array = renters_rooms[renter];
-        string memory room_id = "";
+        
+        uint room_id;
         bool room_found = false;
 
         // Checks if the owner has any rooms available
-        for (uint i = 0; i < rooms_array.length; i++) {
-            if (!rooms_array[i].occupied) {
+        for (uint i = 0; i < user_info[renter].num_rooms; i++) {
+            if (!rooms_record[renters_rooms[renter][i]].occupied) {
                 room_found = true;
-                room_id = rooms_array[i].id;
+                room_id = rooms_record[renters_rooms[renter][i]].id;
                 break;
             }
         }
@@ -121,44 +124,111 @@ contract Prova {
         uint new_id = num_contracts;
         num_contracts++;
         rooms_record[room_id].occupied = true;
-        contract_instance memory instance = contract_instance({contract_id: new_id, room_id: room_id, student_paid: false, renter_paid: false, student: student, renter: renter});
+        contract_instance memory instance = contract_instance({contract_id: new_id, room_id: room_id, student_paid: false, renter_paid: false, amt_paid: 0, student: student, renter: renter});
         contract_record[new_id] = instance;
-        contract_ids.push(new_id);
-        address_record[student].push(new_id);
-        address_record[renter].push(new_id);
+        user_info[student].room_record.push(new_id);
+        user_info[renter].room_record.push(new_id);
     
     }
 
     // customizziamo receive 
     receive() external payable {
+        Role role = user_info[msg.sender].role;
+        require(role == Role.Student, "Unauthorized payment");
+        uint contract_id = user_info[msg.sender].room_record[0];
+        contract_instance memory instance = contract_record[contract_id];
+        uint deposit = rooms_record[instance.room_id].deposit;
         uint paid_amount = msg.value;
-        require(paid_amount == conversion_rate * caparra, "Tu vuole inculale meeee");
-        address payer = msg.sender;
-        uint l = contract_ids.length;
-        for (uint i = 0; i < l; i++) {
-            uint curr_id = contract_ids[i];
-            contract_instance memory curr_contract = contract_record[curr_id];
-            if (curr_contract.student == payer) {
-                curr_contract.student_paid = true;
-                break;
-            }
-            else if (curr_contract.renter == payer) {
-                curr_contract.renter_paid = true;
-            }
-        }
+        require(paid_amount == conversion_rate * deposit, "Tu vuole inculale meeee");
+        
+        instance.amt_paid = paid_amount;
+        instance.student_paid = true;
 
     }
 
- 
-    function check_if_already_in_contract(address student, string calldata room_id) private view returns (bool) {
+
+    function withdraw_from_contract(uint contract_id) public {
+        // IDEA: lo studente non può ritirarsi prima di aver pagato la caparra, oppure può ritirarsi gratuitamente nel lasso di tempo 
+        // che ha per pagare la caparra
+        address receder = msg.sender;
+        address other;
+        contract_instance memory instance = contract_record[contract_id];
+        if (user_info[msg.sender].role == Role.Student) {
+            other = contract_record[contract_id].renter;
+        }
+        else {
+            other = contract_record[contract_id].student;
+        }
+        uint room_id = instance.room_id;
+
+        delete_contract_instance(receder, other, contract_id, room_id);
+        
+        payable(other).transfer(instance.amt_paid);
+    }
+
+    function delete_contract_instance(address a1, address a2, uint contract_id, uint room_id) private {
+        
+        // delete the contract instance
+        delete(contract_record[contract_id]);
+
+        // mark the room as not occupied
+        rooms_record[room_id].occupied = false;
+        
+        // modify the list of contract instances the addresses are a part of
+        uint[] memory a1_record = user_info[a1].room_record;
+        uint[] memory a1_new_record = new uint[](a1_record.length - 1); // la lunghezza è uno in meno perché tolgo l'id di contratto dall'array
+        uint[] memory a2_record = user_info[a2].room_record;
+        uint[] memory a2_new_record = new uint[](a2_record.length - 1);
+
+        
+        for(uint i = 0; i < a1_record.length; i++) {
+            if (a1_record[i] != contract_id) {
+                a1_new_record[i] = a1_record[i];
+            }  
+        }
+        user_info[a1].room_record = a1_new_record; 
+
+        for(uint i = 0; i < a2_record.length; i++) {
+            if (a2_record[i] != contract_id) {
+                a2_new_record[i] = a2_record[i];
+            }  
+        }
+        user_info[a2].room_record = a2_new_record;
+    }
+
+    function end_contract_successfully(uint contract_id) public returns (bool) {
+        //TODO
+    }
+
+
+
+    function get_user_info(address add) public view returns (user memory) {
+        return user_info[add];
+    }
+
+    function get_contract_info(uint256 contract_id) public view returns (contract_instance memory) {
+        return contract_record[contract_id];
+    }
+
+
+    function get_room_info(uint room_id) public view returns (room memory) {
+        return rooms_record[room_id];
+    }
+
+
+    function check_if_already_in_contract(address student, uint room_id) private view returns (bool) {
         // controlla se lo studente o la stanza (disgiunzione inclusiva) sono già in un contratto
         
-        uint[] memory student_array = address_record[student];
+        uint[] memory student_array = user_info[student].room_record;
 
         if (student_array.length > 0 || rooms_record[room_id].occupied == true) {
             return true;
         }
         return false;
 
-    }    
+    }
+
+    function check_user_already_registered(address add) private view returns (bool) {
+        return user_info[add].already_init;
+    }  
 }

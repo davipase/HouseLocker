feff392ce5c2fe1e0da1b4d7e477bce60452fdd2
davipase <davipase@gmail.com>
2023-06-22 17:49:35 +0200

    prove?

--- smart_contracts/zkp/migrations/1_deploy_all.js
+++
@@ -1,9 +0,0 @@
-var accountVerification = artifacts.require("./accountVerification.sol");
-var secp = artifacts.require("./Secp256k1.sol");
-var zkp = artifacts.require("./zkp.sol")
-
-module.exports = function(deployer){
-    deployer.link(accountVerification, secp);
-    deployer.link(accountVerification,zkp)
-    deployer.deploy(accountVerification);
-};
